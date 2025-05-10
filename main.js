18d4f3b301c39bb874212df6a51a144e4504a6b5
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-10 15:12:41 +0200

    Create functions for getting choices

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random()*3)); // 0, 1 or 2
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";
+        default:
+            throw new Error('Invalid randomNumber for computerChoice');
+    }
+}
+
+function getUserChoice() {
+    let userChoice = prompt("What is your choice? \n (Enter 'rock', 'paper' or 'scissor'): ");
+    if ((userChoice === "rock") || (userChoice === 'paper') || (userChoice === 'scissor')) {
+        return userChoice;
+    }
+    return;
+}
+
+console.log(getComputerChoice());
+console.log(getUserChoice());
