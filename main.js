92ee54200eca1d8a93304a6921c8d555fb3100c6
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-10 16:34:50 +0200

    Create functions for the comparison of the choices

--- main.js
+++ main.js
@@ -7,19 +7,84 @@ function getComputerChoice() {
         case 1:
             return "paper";
         case 2:
-            return "scissor";
+            return "scissors";
         default:
             throw new Error('Invalid randomNumber for computerChoice');
     }
 }
 
-function getUserChoice() {
-    let userChoice = prompt("What is your choice? \n (Enter 'rock', 'paper' or 'scissor'): ");
-    if ((userChoice === "rock") || (userChoice === 'paper') || (userChoice === 'scissor')) {
-        return userChoice;
+function getUserChoice(text) {
+    let userChoice = prompt(text);
+    while ((userChoice !== "rock") && (userChoice !== 'paper') && (userChoice !== 'scissors')) {
+        userChoice = prompt("Your choice was invalid. Please enter 'rock', 'paper' or 'scissors'.");
     }
-    return;
+    return userChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getUserChoice());
+function playOneRound(score1, score2) {
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice("What is your choice? \n(Please enter 'rock', 'paper' or 'scissors'.");
+    let result = compareChoices(computerChoice, userChoice);
+    switch (result) {
+        case 0:
+            tie();
+            break;
+        case 1:
+            win(1);
+            break;
+        case 2:
+            win(2);
+            break;
+        default:
+            throw new Error("The comparison of the two values went wrong.");
+    }
+    alert("hello");
+}
+
+function compareChoices(choice1, choice2) {
+    if (choice1 === "rock") {
+        if (choice2 === "rock") {
+            return 0;
+        }
+        if (choice2 === "scissor") {
+            return 1;
+        }
+        return 2;
+    }
+    if (choice1 === "paper") {
+        if (choice2 === "paper") {
+            return 0;
+        }
+        if (choice2 === "rock") {
+            return 1;
+        }
+        return 2;
+    }
+    if (choice1 === "scissors") {
+        if (choice2 === "scissors") {
+            return 0;
+        }
+        if (choice2 === "paper") {
+            return 1;
+        }
+        return 2;
+    }
+}
+
+function tie() {
+    alert("It's a tie!");
+}
+
+function win(number) {
+    if (number == 1) {
+        alert("The computer wins!");
+        return;
+    }
+    if (number === 2) {
+        alert("You win!");
+        return;
+    }
+    throw new Error("The win() function was invoked with a invalid argument.");
+}
+
+playOneRound();
